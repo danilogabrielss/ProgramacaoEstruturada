3adcafe51e3093f70dde69c9517e0dbad2ac67b1
Danilo Gabriel <59902770+danilogabrielss@users.noreply.github.com>
2020-08-17 20:31:09 -0300

    ex 13feito

---
+++ ex13/ex13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vf, vi, aceleracao, tempo, ms;
+            Console.Write("Digite a velocidade incial (em m/s): ");
+            vi = double.Parse(Console.ReadLine());
+            Console.Write("Digite o valor da aceleracao: ");
+            aceleracao = double.Parse(Console.ReadLine());
+            Console.Write("Digite o tempo (em segundos): ");
+            tempo = double.Parse(Console.ReadLine());
+            ms = (tempo * aceleracao) + tempo ;
+            vf = ms * 3.6;
+            Console.Write("A velociade final é " + vf +  " (em km/h)");
+            Console.ReadKey();
+        }
+    }
+}
--- ex27/ex27/Program.cs
+++ ex27/ex27/Program.cs
@@ -6,7 +6,24 @@ namespace ex27
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double catetooposto, catetoadj, hipo, somacat, quadhip;
+            Console.Write("Digite o valor de A: ");
+            catetoadj = double.Parse(Console.ReadLine());
+            Console.Write("Digite o valor de B: ");
+            catetooposto = double.Parse(Console.ReadLine());
+            Console.Write("Digite o valor de C: ");
+            hipo = double.Parse(Console.ReadLine());
+            quadhip = hipo * hipo;
+            somacat = (catetooposto * catetooposto) + (catetoadj * catetoadj);
+            if (somacat == quadhip)
+            {
+                Console.Write("É um triangulo retangulo");
+            }
+            else
+            {
+                Console.Write("Não é um triangulo retangulo");
+            }
+            Console.Read();
         }
     }
 }
