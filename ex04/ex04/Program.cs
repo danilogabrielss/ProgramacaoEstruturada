73cbb590c687348b5a5cc24e0854c8af2a0d6359
Danilo Gabriel <59902770+danilogabrielss@users.noreply.github.com>
2020-08-22 14:55:29 -0300

    exercicios em txt

--- ex04/ex04/Program.cs
+++ ex04/ex04/Program.cs
@@ -10,6 +10,14 @@ namespace ex04
     {
         static void Main(string[] args)
         {
+            int bases, altura, area ; 
+            Console.Write("Digite o valor da base: ");
+            bases = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o valor da altura: ");
+            altura = Convert.ToInt32(Console.ReadLine());
+            area = (bases * altura) / 2 ;
+            Console.Write("O valor da area do triangulo é " + area);
+            Console.ReadKey();
         }
     }
 }
